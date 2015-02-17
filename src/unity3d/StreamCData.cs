6dc7391147028a6fde2042e99198d81ebd7a6e50
Tomohiro Teranishi <tteranishi@oneup-inc.com>
2015-02-17 20:43:22 +0900

    adding unit client code

---
+++ src/unity3d/StreamCData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StreamCData  {
+
+	private Dictionary<string,object> header;
+	private Dictionary<string,object> body;
+
+	public StreamCData(){}
+
+	public void SetHeader(Dictionary<string,object> d){
+		this.header = d;
+	}
+	public void SetBody(Dictionary<string,object> d){
+		this.body = d;
+	}
+
+	public string GetCMD(){
+		return (string)this.header["CMD"];
+	}
+
+	public Dictionary<string,Dictionary<string,object>> GetData(){
+		Dictionary<string,Dictionary<string,object>> data = new Dictionary<string,Dictionary<string,object>>();
+		data["H"] = this.header;
+		data["B"] = this.body;
+		return data;
+	}
+
+}
---
+++ src/unity3d/StreamClient.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Threading;
+using MsgPack;
+using MsgPack.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class StreamClient {
+
+	private TcpClient client;
+	private bool threadOK = true;
+
+	public StreamClient(){}
+
+	public void Connect(string host,int port){
+		this.client = new TcpClient(host,port);
+
+		Thread thread = new Thread( () =>Receiver());
+		thread.Start ();
+		
+
+	}
+	
+	public void Close(){
+		this.threadOK  = false;
+		if(this.client != null ){
+
+			NetworkStream stream = this.client.GetStream();
+			if (stream != null) {
+				stream.Close ();
+				stream = null;
+			}
+			this.client.Close ();
+			this.client = null;
+		}
+		Debug.Log ("Closed");
+	}
+
+	public void Send(StreamCData cdata){
+
+		MemoryStream stream = new MemoryStream();
+		var serializer = MessagePackSerializer.Get<Dictionary<string, Dictionary<string, object>>>();
+		serializer.Pack(stream, cdata.GetData ());
+
+		this.client.GetStream().Write(stream.GetBuffer(),0,(int)stream.Length);
+
+	}
+
+	private void Receiver(){
+
+		while(this.threadOK ){
+
+			while (this.client.Available == 0)
+			{
+				Thread.Sleep(10);
+			}
+
+			try
+			{
+				MessagePackObject mpo = Unpacking.UnpackObject( this.client.GetStream() );
+				Debug.Log(mpo);
+
+			}
+			catch( UnpackException  )
+			{
+				Thread.Sleep(10);
+			}
+
+			
+		}
+	}
+
+}
+
+
